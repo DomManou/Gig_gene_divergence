bd9174e73c473b510afce38b085d5c3eabdfe7dd
DomnikiM <domniki.manousi@nmbu.no>
2025-04-03 08:42:27 +0200

    edited figure plots
    sorted figure 3 and 4 genes, modified palette colors

--- functional_domain_analysis_figure3-4.R
+++ functional_domain_analysis_figure3-4.R
@@ -29,20 +29,16 @@ clusters<-pfc %>% filter(grepl('ENSSSAG',gene)) %>% inner_join(.,gigs,by=c('gene
 g1cpf<-clusters %>% filter(gfam=="gig1") %>% 
   transmute(gene=ggroup,cluster=gclade,chrom,
             db=V4,db.id=V5,db.desc=V6,pval=as.numeric(V9),aa.start=V7,aa.stop=V8,aa=V3) %>% 
-  arrange(cluster,chrom)
+  arrange(cluster,chrom,gene)
 
 g2cpf<-clusters %>% filter(gfam=="gig2") %>% 
   transmute(gene=ggroup,cluster=gclade,chrom,
             db=V4,db.id=V5,db.desc=V6,pval=as.numeric(V9),aa.start=V7,aa.stop=V8,aa=V3) %>% 
-  arrange(cluster,chrom)
+  arrange(cluster,chrom,gene)
 
 #how many in gig1, how many in gig2?
 g1cpf %>% distinct(db.desc,db.id,db)
 g2cpf %>% distinct(db.desc,db.id,db)
-
-
-#arrange everything according to gig cluster and chromosomal arrangement 
-g1cpf %>% arrange(cluster,chrom)
 
 
 #assign functional clusters 
@@ -113,7 +109,9 @@ fmfg2<-mfg2 %>% inner_join(.,filtgig2[,c(1:3,10)],relationship="many-to-many") %
 #make custom palette
 df_fg2 <- mfg2 %>% group_by(db.desc) %>% dplyr::count() 
 df_fg1 <- mfg1 %>% group_by(db.desc) %>% dplyr::count()
-palette <- c("#fca311","#a7c957","#4f772d","#fb6f92","#bc6c25","#48cae4","#c77dff","#d00000","grey","yellow3")
+palette <- c("#fca311","#108720","#a7c957","#005f99","#fb6f92","#48cae4","#c77dff","#d00000","grey","yellow3")
+
+
 levels <- union(levels(factor(df_fg1$db.desc)), levels(factor(df_fg2$db.desc)))
 names(palette) <- levels
 
@@ -131,7 +129,7 @@ ssag2s<-ssag2gn %>% transmute(file_id="Salmo salar",seq_id,seq_desc=name,length=
   arrange(seq_id) %>% as.data.frame()
 
 #make sure the genes are in proper arrangement
-g1gn<-clusters %>% filter(gfam=="gig1") %>% distinct(ggroup,gclade,chrom) %>% arrange(gclade,chrom)
+g1gn<-clusters %>% filter(gfam=="gig1") %>% distinct(ggroup,gclade,chrom) %>% arrange(gclade,chrom,ggroup)
 
 ssag1gn$seq_id<-factor(ssag1gn$seq_id,levels = unique(rev(g1gn$ggroup)))
 ssag1s$seq_id<-factor(ssag1s$seq_id,levels = unique(rev(g1gn$ggroup)))
@@ -143,13 +141,13 @@ sg1s<-ssag1s %>% arrange(seq_id)
 ssag1<-gggenomes(genes = sg1g,seqs=sg1s) + 
   geom_seq() + 
   geom_gene(aes(fill=name),position="pile") +
-  geom_bin_label() +
+  geom_bin_label(size=4) +
   scale_fill_manual(values = palette) +
   labs(fill="Predicted functional clusters") 
 
 
 #gig2
-g2gn<-clusters %>% filter(gfam=="gig2") %>% distinct(ggroup,gclade,chrom) %>% arrange(gclade,chrom)
+g2gn<-clusters %>% filter(gfam=="gig2") %>% distinct(ggroup,gclade,chrom) %>% arrange(gclade,chrom,ggroup)
 ssag2gn$seq_id<-factor(ssag2gn$seq_id,levels = unique(rev(g2gn$ggroup)))
 ssag2s$seq_id<-factor(ssag2s$seq_id,levels = unique(rev(g2gn$ggroup)))
 
@@ -159,24 +157,26 @@ sg2s<-ssag2s %>% arrange(seq_id)
 ssag2<-gggenomes(genes = sg2g,seqs=sg2s) + 
   geom_seq() + 
   geom_gene(aes(fill=name),position="pile") + 
-  geom_bin_label() +
+  geom_bin_label(size=4) +
   scale_fill_manual(values = palette) +
   labs(fill="Predicted functional cluster") +
   theme(axis.text.y = element_blank(),axis.ticks.y = element_blank(),axis.title.y = element_blank())  
 
 
-#make figure 3 plots
+#plots
 ssag1
 ssag2
 
 
 
-#Figure 4 components 
-dtpg<-ssag2gn %>% filter(seq_id=="gig2B_ssa02d" | seq_id=="gig2B_ssa12a" | seq_id=="gig2B_ssa12b")
-dtps<-ssag2s %>% filter(seq_id=="gig2B_ssa02d" | seq_id=="gig2B_ssa12a" | seq_id=="gig2B_ssa12b")
+#make the gene plots and dotplots in figure 4  
+
+dtpg<-ssag2gn %>% filter(seq_id=="gig2B_ssa02d" | seq_id=="gig2B_ssa12a" | seq_id=="gig2B_ssa12b") %>% arrange(seq_id)
+dtps<-ssag2s %>% filter(seq_id=="gig2B_ssa02d" | seq_id=="gig2B_ssa12a" | seq_id=="gig2B_ssa12b") %>% arrange(seq_id)
+
 
 gggenomes(genes = dtpg,seqs=dtps) + geom_seq() + geom_gene(aes(fill=name),position="pile") +
-  geom_bin_label() +  scale_fill_manual(values = palette) + labs(fill="Predicted functional clusters") 
+  geom_bin_label(size=4) +  scale_fill_manual(values = palette) + labs(fill="Predicted functional clusters")
 
 library(dotplot)
 library(seqinr)
